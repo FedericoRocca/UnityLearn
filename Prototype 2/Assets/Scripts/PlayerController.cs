ed1ef835b221ee3709fe718ad7b0b4117d390b37
federico.rocca <federico.rocca@bbva.com>
2024-07-10 18:04:39 -0300

    t push Import de project 2 + lesson 2.1 players position

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 15.0f;
+    public float xRange = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        try
+        {
+            
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(ex);
+            throw;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            if(transform.position.x <= -xRange)
+            {
+                transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            }
+            if (transform.position.x >= xRange)
+            {
+                transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            }
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime); 
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(ex);
+            throw;
+        }
+    }
+        
+}
